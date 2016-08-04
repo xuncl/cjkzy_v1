e5d8fb2a1293317415d75e3840162266d64de69c
lxcool91 <715814509@qq.com>
2016-08-04 09:17:04 +0800

    test 2

--- Application/Views/Controller/IndexController.class.php
+++ Application/Views/Controller/IndexController.class.php
@@ -26,6 +26,6 @@ class IndexController extends Controller {
 //        $fetchContent = str_replace("l","lllll",$fetchContent);
         $this->show($fetchContent);
 
-        echo "xxxxx";
+        echo "xx";
     }
 }
