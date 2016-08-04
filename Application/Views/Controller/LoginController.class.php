df8c40067019aa2134cd35b224258284231543d5
xuncl <xuncl@126.com>
2016-08-04 13:12:42 +0800

    login page update

--- Application/Views/Controller/LoginController.class.php
+++ Application/Views/Controller/LoginController.class.php
@@ -12,14 +12,15 @@ class LoginController extends Controller {
     //注册表单体检处理页面
     public function register_do()
     {
-        //dump($_POST);
-        $data['phone_number'] = I('post.phone_number','','strip_tags');
-        $data['role_id'] = I('post.type','','strip_tags');
-        $res = M('users')->add($data);
-        $_SESSION['user_info'] = $res;
-        if($res)
-        {
-            echo $ajax_ress = 0;
-        }
+        dump($_POST);
+
+//        $data['phone_number'] = I('post.phone_number','','strip_tags');
+//        $data['role_id'] = I('post.type','','strip_tags');
+//        $res = M('users')->add($data);
+//        $_SESSION['user_info'] = $res;
+//        if($res)
+//        {
+//            echo $ajax_ress = 0;
+//        }
     }
 }
